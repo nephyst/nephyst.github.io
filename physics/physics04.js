c507b94ab55d5c70d0150fca9110d869483cfc00
Anthony Trupiano <orangecrush@gmail.com>
2015-11-26 18:45:38 +0700

    Updated planet demo;

--- physics/physics04.js
+++ physics/physics04.js
@@ -6,12 +6,16 @@ window.onload = function() {
     sun = Particle.create(width / 2, height / 2, 0, 0),
     planets = [];
 
-  sun.mass = 17500;
+  sun.mass = 15000;
 
   for (var i = 0; i < 50; i++) {
-    planets.push(Particle.create(width / 2 + Math.random() * 150 + 300, height / 2, Math.random() * 5 + 4, -Math.PI / 2));
+    planets.push(Particle.create(
+      width / 2 + Math.random() * width / 5 + 200,
+      height / 2 + Math.random() * height / 4 - 100,
+      Math.random() * 4 + 6,
+      -Math.PI / 2));
     planets[i].color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
-    planets[i].size = Math.random() * 15 + 7;
+    planets[i].size = Math.random() * 12 + 3;
     planets[i].mass = planets[i].size * planets[i].size;
   }
 
