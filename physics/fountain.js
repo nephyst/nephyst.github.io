c4f795371f81eb43ad18a37423fb6860bcb6babe
Anthony Trupiano <orangecrush@gmail.com>
2016-02-18 23:02:59 -0800

    more bernie

--- physics/fountain.js
+++ physics/fountain.js
@@ -6,12 +6,15 @@ window.onload = function() {
     particles = [],
     gravity = 0.25;
 
+  var bernie = new Image();
+  bernie.src = 'http://i.imgur.com/B7G8S6o.png';
+
   update();
 
   function update() {
     c.clearRect(0, 0, width, height);
 
-    if (particles.length < 500) {
+    if (particles.length < 200) {
       particles.push(createParticle());
     }
 
@@ -24,7 +27,7 @@ window.onload = function() {
         particle.vx = particle.vx * 0.97;
         particle.vy = -particle.vy * 0.5;
         particle.y = height - particle.radius + 1;
-        particle.radius -= 0.05;
+        particle.radius -= 0.3;
       }
       if (particle.radius < 0.06) {
         particle = createParticle();
@@ -33,16 +36,19 @@ window.onload = function() {
 
       c.beginPath();
       c.fillStyle = particle.color;
-      c.arc(particle.x, particle.y, particle.radius, 0, Math.PI * 2, false);
+      c.arc(particle.x, particle.y, particle.radius * 0.9, 0, Math.PI * 2, false);
+      c.globalAlpha = 1;
       c.fill();
+      c.globalAlpha = 0.7;
+      c.drawImage(bernie, particle.x - particle.radius, particle.y - particle.radius, particle.radius * 2, particle.radius * 2);
     }
     requestAnimationFrame(update);
   }
 
   function createParticle() {
-    var particle = Particle.create(width / 2, height - 25, randomRange(8.5, 18.5), -Math.PI / 2 + randomRange(-0.13, 0.13));
+    var particle = Particle.create(width / 2, height - 25, randomRange(13, 20), -Math.PI / 2 + randomRange(-0.13, 0.13));
     particle.color = randomColor();
-    particle.radius = randomRange(3, 11);
+    particle.radius = randomRange(5, 40);
     return particle;
   }
 
