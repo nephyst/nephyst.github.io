7d080f67fa0b2b1c7f7f561ffc6df9fbdfdcc1ad
Anthony Trupiano <orangecrush@gmail.com>
2016-08-12 15:37:14 -0700

    draw sprite boundaries

--- animation/sprites.js
+++ animation/sprites.js
@@ -1,9 +1,12 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-var sprites = [];
+var images = [];
+var selectedImage = -1;
 var imageInput = $('#imageInput');
-var spriteList = $('#imageList');
+var imageList = $('#imageList');
+var spriteWidth = $('#spriteWidth');
+var spriteHeight = $('#spriteHeight');
 
 // Canvas background pattern
 var pattern = document.createElement('canvas');
@@ -20,36 +23,58 @@ resetCanvas();
 imageInput.on('change', function (e) {
     var file = e.target.files[0];
 
-    var sprite = {};
-    sprites.push(sprite);
-    sprite.imageFile = new Image();
-    sprite.fileName = file.name;
+    var image = {};
+    images.push(image);
+    image.fileName = file.name;
+    //defaults
+    image.imageFile = new Image();
+    image.spriteWidth = 32;
+    image.spriteHeight = 32;
 
     var reader = new FileReader();
     reader.onload = function (fileResult) {
-        sprite.imageFile.src = fileResult.target.result;
-        refreshSpriteList();
+        image.imageFile.src = fileResult.target.result;
+        refreshImageList();
     };
     reader.readAsDataURL(file);
 });
 
-function refreshSpriteList() {
-    spriteList.empty();
-    sprites.forEach(function (sprite, i) {
-        spriteList
+spriteWidth.on('change textInput input', function () {
+    var image = images[selectedImage];
+    image.spriteWidth = parseInt($(this).val());
+    drawUniformBoundaries();
+});
+
+spriteHeight.on('change textInput input', function (e) {
+    var image = images[selectedImage];
+    image.spriteHeight = parseInt($(this).val());
+    drawUniformBoundaries();
+});
+
+function refreshImageList() {
+    imageList.empty();
+    images.forEach(function (image, i) {
+        imageList
             .append($('<option>', {value: i})
-                .text(sprite.fileName));
+                .text(image.fileName));
     });
-    spriteList.attr('size', Math.max(3, sprites.length));
+    imageList.attr('size', Math.max(3, images.length));
 }
 
-spriteList.change(function () {
-    var value = $(this).val();
+imageList.change(function () {
+    selectedImage = $(this).val();
+    var image = images[selectedImage];
 
-    var img = sprites[value].imageFile;
+    var img = image.imageFile;
     resetCanvas(img.width, img.height);
-    console.log(img.width, img.height);
     ctx.drawImage(img, 0, 0);
+
+    $('.imageControls').css('display', 'block');
+    spriteWidth.val(image.spriteWidth);
+    spriteHeight.val(image.spriteHeight);
+
+    drawUniformBoundaries();
+
 });
 
 function resetCanvas(width, height) {
@@ -68,4 +93,29 @@ function resetCanvas(width, height) {
 
     ctx.fillStyle = checkerPattern;
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+function drawUniformBoundaries() {
+    var image = images[selectedImage];
+    var img = image.imageFile;
+    resetCanvas(img.width, img.height);
+    ctx.drawImage(img, 0, 0);
+
+    var spriteWidth = image.spriteWidth;
+    var spriteHeight = image.spriteHeight;
+    var imageWidth = image.imageFile.width;
+    var imageHeight = image.imageFile.height;
+
+    if(spriteWidth <=0 || spriteHeight <= 0) {
+        return;
+    }
+
+    ctx.fillStyle = "rgba(0, 0, 255, 0.3)";
+    ctx.strokeStyle = "rgba(0, 0, 255, 0.7)";
+    for (var y = 0; y < imageHeight; y += spriteHeight) {
+        for (var x = 0; x < imageWidth; x += spriteWidth) {
+            ctx.fillRect(x, y, spriteWidth, spriteHeight);
+            ctx.strokeRect(x, y, spriteWidth, spriteHeight);
+        }
+    }
 }
